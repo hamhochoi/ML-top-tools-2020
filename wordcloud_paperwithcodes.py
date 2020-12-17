6ccfc9e9098243da827622b7a873d3bd28b2140d
hamhochoi <dothanhwork2017@gmail.com>
2020-12-17 18:05:52 +0900

    add wordcloud python file

---
+++ wordcloud_paperwithcodes.py
@@ -0,0 +1,28 @@
+from wordcloud import WordCloud, STOPWORDS 
+import matplotlib.pyplot as plt 
+import pandas as pd 
+
+f = open("top_tools.txt")
+lines = f.readlines()
+
+text = ''
+
+for line in lines:
+	txt, n = line.split(", ")
+	text += (txt+" ")*int(n) + " "
+	
+
+stopwords = set(STOPWORDS) 
+
+wordcloud = WordCloud(width = 800, height = 800, 
+                background_color ='white', 
+                stopwords = stopwords, 
+                min_font_size = 10, collocations=False).generate(text) 
+
+
+plt.figure(figsize = (8, 8), facecolor = None) 
+plt.imshow(wordcloud) 
+plt.axis("off") 
+plt.tight_layout(pad = 0) 
+  
+plt.show() 
